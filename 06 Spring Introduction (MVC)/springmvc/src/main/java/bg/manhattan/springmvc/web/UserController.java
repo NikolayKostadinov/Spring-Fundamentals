5ddd6cc5d499deac7ee750125d904291f67074f2
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-06-02 14:16:58 +0300

    Refactoring

--- 06 Spring Introduction (MVC)/springmvc/src/main/java/bg/manhattan/springmvc/web/UserController.java
+++ 06 Spring Introduction (MVC)/springmvc/src/main/java/bg/manhattan/springmvc/web/UserController.java
@@ -5,22 +5,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/user")
-    public String user(){
+    @GetMapping("/create")
+    public String user() {
         return "create_user";
     }
 
-    @PostMapping("/user")
-    public ModelAndView user(UserDto user){
+    @PostMapping("/add")
+    public ModelAndView user(UserDto user) {
         System.out.println(user);
         ModelAndView result = new ModelAndView();
-        result.addObject("fullName", user.getFullName());
+        result.addObject("full-name", user.getFullName());
+        result.setViewName("redirect:/user/created");
+        return result;
+    }
+
+    @GetMapping("/created")
+    public ModelAndView userCreated(@RequestParam("full-name") String fullLame) {
+        ModelAndView result = new ModelAndView();
+        result.addObject("fullName", fullLame);
         result.setViewName("user_created");
         return result;
     }
